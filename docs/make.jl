ba426f5d5b8d1a1d1302fcbb22fb45281e486c80
Luiz M. Faria <maltezfaria@gmail.com>
2023-10-20 12:44:39 +0200

    tweaks to cholesky example

--- docs/make.jl
+++ docs/make.jl
@@ -3,14 +3,21 @@ using Documenter
 using Literate
 
 for example in ["cholesky", "blur-roberts"]
-    dir = joinpath(DataFlowTasks.PROJECT_ROOT,"docs","src", "examples", example)
+    dir = joinpath(DataFlowTasks.PROJECT_ROOT, "docs", "src", "examples", example)
     src = joinpath(dir, "$(example).jl")
     Literate.markdown(src, dir)
     Literate.notebook(src, dir)
 end
 
+on_CI = get(ENV, "CI", "false") == "true"
+
 DataFlowTasks.stack_weakdeps_env!()
-DocMeta.setdocmeta!(DataFlowTasks, :DocTestSetup, :(using CairoMakie, GraphViz, DataFlowTasks); recursive=true)
+DocMeta.setdocmeta!(
+    DataFlowTasks,
+    :DocTestSetup,
+    :(using CairoMakie, GraphViz, DataFlowTasks);
+    recursive = true,
+)
 
 modules = [DataFlowTasks]
 if isdefined(Base, :get_extension)
@@ -22,14 +29,14 @@ end
 
 makedocs(;
     modules = modules,
-    repo="https://github.com/maltezfaria/DataFlowTasks.jl/blob/{commit}{path}#{line}",
-    sitename="DataFlowTasks.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://maltezfaria.github.io/DataFlowTasks.jl",
-        assets=String[],
+    repo = "",
+    sitename = "DataFlowTasks.jl",
+    format = Documenter.HTML(;
+        prettyurls = on_CI,
+        canonical = "https://maltezfaria.github.io/DataFlowTasks.jl",
+        assets = String[],
     ),
-    pages=[
+    pages = [
         "Getting started" => "index.md",
         "Debugging & Profiling" => "profiling.md",
         "Examples" => [
@@ -41,12 +48,14 @@ makedocs(;
         ],
         # "Comparaison with Dagger.jl" => "dagger.md",
         # "Common Issues" => "issues.md",
-        "References" => "references.md"
+        "References" => "references.md",
     ],
+    warnonly = on_CI ? false : Documenter.except(:linkcheck_remotes),
+    pagesonly = true,
 )
 
 deploydocs(;
-    repo="github.com/maltezfaria/DataFlowTasks.jl",
-    devbranch="main",
-    push_preview=true,
+    repo = "github.com/maltezfaria/DataFlowTasks.jl",
+    devbranch = "main",
+    push_preview = true,
 )
