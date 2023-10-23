1dee8f8561641799b64a90b8a6ec1e3f1fbbb3a6
Documenter.jl <documenter@juliadocs.github.io>
2023-10-23 10:53:18 +0200

    build based on 7a63ab9

--- previews/PR57/readme/README.jl
+++ previews/PR57/readme/README.jl
@@ -214,16 +214,16 @@ A = (A + adjoint(A))/2
 A = A + n*I;
 
 #-
-@info "Testing sequential Cholesky factorization"          #hide
+println("Testing sequential Cholesky factorization")       #hide
 F = cholesky_tiled!(copy(A), ts)                           #hide
                                                            #hide
-## Check results                                           #hide
+## Check results                                            #hide
 err = norm(F.L*F.U-A,Inf)/max(norm(A),norm(F.L*F.U))       #hide
 @show err                                                  #hide
 @assert err < eps(Float64)                                 #hide
 #-
 
-@info "Testing parallel Cholesky factorization" #hide
+println("Testing parallel Cholesky factorization")         #hide
 
 ## First run to trigger compilation
 F = cholesky_dft!(copy(A), ts)
