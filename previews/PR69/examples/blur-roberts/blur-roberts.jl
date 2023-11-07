56d0617ad398663390b915688fd8f262baa9fd08
Documenter.jl <documenter@juliadocs.github.io>
2023-11-07 10:05:56 +0100

    build based on d2f868e

---
+++ previews/PR69/examples/blur-roberts/blur-roberts.jl
@@ -0,0 +1,264 @@
+cd(@__DIR__)             #src
+import Pkg               #src
+Pkg.activate("../../..") #src
+using BenchmarkTools     #src
+
+# # Blur & Roberts image filters
+#
+#md # [![ipynb](https://img.shields.io/badge/download-ipynb-blue)](blur-roberts.ipynb)
+#md # [![nbviewer](https://img.shields.io/badge/show-nbviewer-blue.svg)](@__NBVIEWER_ROOT_URL__/examples/blur-roberts/blur-roberts.ipynb)
+#
+# This example illustrate the use of `DataFlowTasks.jl` to parallelize the tiled
+# application of two kernels used in image processing. The application first
+# applies a blur filter on each pixel of the image; in a second step, the [Roberts
+# cross operator](https://en.wikipedia.org/wiki/Roberts_cross) is applied to
+# detect edges in the image.
+#
+# Let us first load a test image:
+
+using Images
+url = "https://upload.wikimedia.org/wikipedia/commons/c/c3/Equus_zebra_hartmannae_-_Etosha_2015.jpg"
+ispath("test-image.jpg") || download(url, "test-image.jpg")
+img = Gray.(load("test-image.jpg"))
+
+# We start by defining a few helper functions:
+#
+# - the `contract` and `expand` functions manipulate ranges of indices in order
+#   to respectively contract or expand them by a few pixels;
+#
+# - the `img2mat` and `mat2img` convert between a Gray-scale image and a matrix
+#   of floating-point pixel intensities. The filters will work on this latter
+#   representation, which may need a renormalization to be converted back to a
+#   Gray-scale image.
+
+contract(range,n) = range[begin+n:end-n]
+expand(range,n)   = range[begin]-n:range[end]-n
+
+function img2mat(img)
+    PixelType = eltype(img)
+    mat = Float64.(img)
+    return (PixelType, mat)
+end
+
+function mat2img(PixelType, mat)
+    m1, m2 = extrema(mat)
+    PixelType.((mat .- m1) ./ (m2-m1))
+end
+
+PixelType, mat = img2mat(img);
+
+# ## Filters implementation
+#
+# The `blur!` function averages the value of each pixel with the values of all
+# pixels less than `width` pixels away in manhattan distance. In order to
+# simplify the implementation, the filter is applied only to pixels that are
+# sufficiently far from the boundary to have all their neighbors correctly
+# defined.
+#
+# Results are written in-place in a pre-allocated `dest` array. Unless otherwise
+# specified, the filter is applied to the whole image, but can be reduced to a
+# tile if a smaller `range` argument is provided.
+
+function blur!(dest, src; range=axes(src), width)
+    ri, rj = intersect.(range, contract.(axes(src), width))
+
+    weight = 1/(2*width+1)^2
+    @inbounds for i in ri, j in rj
+        dest[i,j] = 0
+        for δi in -width:width, δj in -width:width
+            dest[i,j] += src[i+δi, j+δj]
+        end
+        dest[i,j] *= weight
+    end
+end
+
+# In the following, we'll use a filter width of 5 pixels, which produces the
+# following results on the test image:
+
+width = 5
+blurred = zero(mat)
+
+blur!(blurred, mat; width)
+
+mat2img(PixelType, blurred)
+
+#  The `roberts!` function applies the Roberts cross operator to the provided
+#  image. Like above, it operates by default on all pixels in the image
+#  (provided they are sufficiently far from the boundaries), but can be
+#  restricted to work on a tile if the `range` argument is provided.
+
+function roberts!(dest, src; range=axes(src))
+    ri, rj = intersect.(range, contract.(axes(src), 1))
+
+    for i in ri, j in rj
+        dest[i,j] = (
+            + (sqrt(src[i,  j]) - sqrt(src[i+1,j+1]))^2
+            + (sqrt(src[i+1,j]) - sqrt(src[i  ,j+1]))^2
+        )^(0.25)
+    end
+end
+
+# Applying this edge detection filter on the original image produces the
+# following results:
+
+contour = zero(mat)
+roberts!(contour, mat)
+
+mat2img(PixelType, contour)
+
+# Chaining the blur and roberts filters may make edge detection less noisy:
+
+function blur_roberts!(img; width, tmp=zero(img))
+    blur!(tmp, img; width)
+    roberts!(img, tmp)
+end
+
+mat1 = copy(mat)
+tmp  = zero(mat)
+
+blur_roberts!(mat1; width, tmp)
+mat2img(PixelType, mat1)
+
+# The elapsed time in this sequential version will serve as reference to
+# evaluate the performance of other implementations:
+
+using BenchmarkTools
+t_seq = @belapsed blur_roberts!(x, width=$width, tmp=$tmp) setup=(x=copy(mat)) evals=1
+
+# ## Tiled filter application
+#
+# The [`TiledIteration.jl`](https://github.com/JuliaArrays/TiledIteration.jl)
+# package implements various tools allowing to define and iterate over disjoint
+# tiles of a larger array. We'll use it to apply the filters tile by tile.
+#
+# The `map_tiled!` higher-order function automates the application of a filter
+# `fun!` on all pixels of an image `src` decomposed with a tilesize `ts`. This
+# higher-order function is then used to define tiled versions of the blur and
+# roberts filters.
+
+using TiledIteration
+
+function map_tiled!(fun!, dest, src, ts)
+    for tile in TileIterator(axes(src), (ts, ts))
+        fun!(dest, src, tile)
+    end
+end
+
+blur_tiled!(dest, src, ts; width) = map_tiled!(dest, src, ts) do dest, src, tile
+    blur!(dest, src; width, range=tile)
+end
+
+roberts_tiled!(dest, src, ts) = map_tiled!(dest, src, ts) do dest, src, tile
+    roberts!(dest, src; range=tile)
+end
+
+function blur_roberts_tiled!(img, ts; width, tmp=zero(img))
+    blur_tiled!(tmp, img, ts; width)
+    roberts_tiled!(img, tmp, ts)
+end
+
+# Decomposing the original image in tiles of size $512\times 512$, the tiled
+# application of the filters yields the same result as above:
+
+ts = 512
+
+mat1 .= mat
+blur_roberts_tiled!(mat1, ts; width, tmp)
+
+mat2img(PixelType, mat1)
+
+# Depending on the system, the fact that memory is now accessed in blocks may
+# (or may not) have a significant impact on the performance, due to cache
+# effects.
+
+t_tiled = @belapsed blur_roberts_tiled!(x, ts; width=$width, tmp=$tmp) setup=(x=copy(mat)) evals=1
+
+# ## Parallel filter application
+
+# Parallelizing the tiled filter application is relatively straightforward using
+# `DataFlowTasks.jl`. As usual, it involves specifying which data is accessed by
+# each task.
+
+using DataFlowTasks
+
+function blur_dft!(dest, src, ts; width)
+    map_tiled!(dest, src, ts) do dest, src, tile
+        outer = intersect.(expand.(tile, width), axes(src))
+        @dspawn begin
+            @R view(src, outer...)
+            @W view(dest, tile...)
+            blur!(dest, src; width, range=tile)
+        end label="blur ($tile)"
+    end
+    @dspawn @R(dest) label="blur (result)"
+end
+
+function roberts_dft!(dest, src, ts)
+    map_tiled!(dest, src, ts) do dest, src, tile
+        outer = intersect.(expand.(tile, 1), axes(src))
+        @dspawn begin
+            @R view(src, outer...)
+            @W view(dest, tile...)
+            roberts!(dest, src; range=tile)
+        end label="roberts ($tile)"
+    end
+    @dspawn @R(dest) label="roberts (result)"
+end
+
+# Note how each filter spawns one task for each tile, and an extra task to get
+# the results in the end. This allows applying a given filter independently of
+# the other.
+#
+# However, the filters remain composable: when applying both filters one after
+# the other, no implicit synchronization is enforced at the end of the blurring
+# stage, and the runtime may decide to intersperse blurring and roberts tasks
+# (as long as the blurring of a tile and all its neighbors is performed before
+# the application of the roberts filter on this tile).
+
+function blur_roberts_dft!(img, ts; width, tmp=zero(img))
+    blur_dft!(tmp, img, ts; width)
+    roberts_dft!(img, tmp, ts)
+    @dspawn @R(img) label="result"
+end
+
+# Again this yields the same results on the test image:
+
+mat1 .= mat;
+blur_roberts_dft!(mat1, ts; width, tmp) |> wait
+
+mat2img(PixelType, mat1)
+
+#-
+
+t_dft = @belapsed wait(blur_roberts_dft!(x, ts; width=$width, tmp=$tmp)) setup=(x = copy(mat)) evals=1
+
+# ## Performance analysis
+
+DataFlowTasks.stack_weakdeps_env!()
+using CairoMakie
+
+barplot([t_seq, t_tiled, t_dft],
+        axis = (; title = "Elapsed time [s]",
+                xticks=(1:3, ["sequential", "tiled", "DataFlowTasks"])))
+
+# A comparison of the performances of all implementations shows that the
+# DataFlowTasks-based implementation produces a good speedup:
+
+(;
+ nthreads = Threads.nthreads(),
+ speedup = t_seq / t_dft)
+
+# We can gain more insight by collecting profiling data:
+
+GC.gc()
+mat1 .= mat;
+log_info = DataFlowTasks.@log wait(blur_roberts_dft!(mat1, ts; width, tmp))
+DataFlowTasks.describe(log_info)
+
+# The parallel trace shows how blur and roberts tasks are interspersed in the time line:
+
+trace = plot(log_info, categories=["blur", "roberts"])
+
+# In terms of performance, elapsed time seems to be bounded in this case by the
+# total computing time of all threads. Re-running the same computation with more
+# threads may help reduce the overall wall-clock time.
---
+++ previews/PR69/examples/cholesky/cholesky.jl
@@ -0,0 +1,232 @@
+cd(@__DIR__)             #src
+import Pkg               #src
+Pkg.activate("../../..") #src
+
+# # [Tiled Cholesky Factorization](@id tiledcholesky-section)
+#
+#md # [![ipynb](https://img.shields.io/badge/download-ipynb-blue)](cholesky.ipynb)
+#md # [![nbviewer](https://img.shields.io/badge/show-nbviewer-blue.svg)](@__NBVIEWER_ROOT_URL__/examples/cholesky/cholesky.ipynb)
+#
+# We illustrate here the use of `DataFlowTasks` to parallelize a tiled Cholesky
+# factorization. The implementation shown here is delibarately made as simple
+# and self-contained as possible; a more complex and more efficient
+# implementation can be found in the
+# [TiledFactorization](https://github.com/maltezfaria/TiledFactorization)
+# package.
+
+#-
+
+# The Cholesky factorization algorithm takes a symmetric positive definite
+# matrix $A$ and finds a lower triangular matrix $L$ such that $A = LLᵀ$. The
+# tiled version of this algorithm decomposes the matrix $A$ into tiles (of even
+# sizes, in this simplified version). At each step of the algorithm, we do a
+# Cholesky factorization on the diagonal tile, use a triangular solve to update
+# all of the tiles at the right of the diagonal tile, and finally update all the
+# tiles of the submatrix with a schur complement.
+#
+# If we have a matrix $A$ decomposed in $n \times n$ tiles, then the algorithm will
+# have $n$ steps. The $i$-th step (with $i \in [1:n]$) performs:
+#
+# -  $1$ cholesky factorization of the $(i,i)$ tile,
+# -  $(i-1)$ triangular solves (one for each tile in the $i$-th row of the upper triangular matrix),
+# -  $i(i-1)/2$ matrix multiplications to update the submatrix.
+#
+# These are the basic operations on tiles, which we are going to spawn in
+# separate tasks in the parallel implementation. Accounting for all iterations,
+# this makes a total of $\mathcal{O}(n^3)$ such tasks, decomposed as:
+#
+# -  $\mathcal{O}(n)$ cholesky factorizations,
+# -  $\mathcal{O}(n^2)$ triangular solves,
+# -  $\mathcal{O}(n^3)$ matrix multiplications.
+
+#-
+
+# The following image illustrates the 2nd step of the algorithm:
+#
+# ![](Cholesky_2ndStep.png)
+
+#-
+
+# ## Sequential implementation
+#
+# A sequential tiled factorization algorithm can be implemented as:
+
+using LinearAlgebra
+BLAS.set_num_threads(1) #src
+
+tilerange(ti, ts) = (ti-1)*ts+1:ti*ts
+
+function cholesky_tiled!(A, ts)
+    m = size(A, 1); @assert m==size(A, 2)
+    m%ts != 0 && error("Tilesize doesn't fit the matrix")
+    n = m÷ts  # number of tiles in each dimension
+
+    T = [view(A, tilerange(i, ts), tilerange(j, ts)) for i in 1:n, j in 1:n]
+
+    for i in 1:n
+        ## Diagonal cholesky serial factorization
+        cholesky!(T[i,i])
+
+        ## Left tiles update
+        U = UpperTriangular(T[i,i])
+        for j in i+1:n
+            ldiv!(U', T[i,j])
+        end
+
+        ## Submatrix update
+        for j in i+1:n
+            for k in j:n
+                mul!(T[j,k], T[i,j]', T[i,k], -1, 1)
+            end
+        end
+    end
+
+    ## Construct the factorized object
+    return Cholesky(A, 'U', zero(LinearAlgebra.BlasInt))
+end
+
+# Let us build a small test case to check the correctness of the
+# factorization. Here we divide a matrix of size 4096×4096 in 8×8 tiles of size
+# 512×512:
+
+n  = 4096
+ts = 512
+A = rand(n, n)
+A = (A + adjoint(A))/2
+A = A + n*I;
+
+# and the results seem to be correct:
+
+@info "Testing sequential Cholesky factorization" #src
+F = cholesky_tiled!(copy(A), ts)
+GC.gc(); @time cholesky_tiled!(copy(A), ts) #src
+
+## Check results
+err = norm(F.L*F.U-A,Inf)/max(norm(A),norm(F.L*F.U))
+@show err
+@assert err < eps(Float64)
+
+#-
+
+# ## Parallel implementation
+#
+# In order to parallelize the code with `DataFlowTasks.jl`, function calls
+# acting on tiles are wrapped within `@dspawn`, along with annotations
+# describing data access modes. We also give meaningful labels to the tasks,
+# which will help debug and profile the code.
+
+using DataFlowTasks
+
+function cholesky_dft!(A, ts)
+    m = size(A, 1); @assert m==size(A, 2)
+    m%ts != 0 && error("Tilesize doesn't fit the matrix")
+    n = m÷ts  # number of tiles in each dimension
+
+    T = [view(A, tilerange(i, ts), tilerange(j, ts)) for i in 1:n, j in 1:n]
+
+    for i in 1:n
+        ## Diagonal cholesky serial factorization
+        @dspawn cholesky!(@RW(T[i,i])) label="chol ($i,$i)"
+
+        ## Left tiles update
+        U = UpperTriangular(T[i,i])
+        for j in i+1:n
+            @dspawn ldiv!(@R(U)', @RW(T[i,j])) label="ldiv ($i,$j)"
+        end
+
+        ## Submatrix update
+        for j in i+1:n
+            for k in j:n
+                @dspawn mul!(@RW(T[j,k]), @R(T[i,j])', @R(T[i,k]), -1, 1) label="schur ($j,$k)"
+            end
+        end
+    end
+
+    ## Construct the factorized object
+    r = @dspawn Cholesky(@R(A), 'U', zero(LinearAlgebra.BlasInt)) label="result"
+    return fetch(r)
+end
+
+# Again, let us check the correctness of the result:
+
+@info "Testing parallel Cholesky factorization on $(Threads.nthreads()) threads" #src
+F = cholesky_dft!(copy(A), ts)
+
+## Check results
+err = norm(F.L*F.U-A,Inf)/max(norm(A),norm(F.L*F.U))
+@show err
+@assert err < eps(Float64)
+
+
+# ## Debugging and Profiling
+#
+# Let us now check what happens during a parallel run of our cholesky
+# factorization. Thanks to the test above, the code is now compiled. Let's re-run it and collect
+# meaningful profiling information:
+
+## Clean profiling environment
+GC.gc()
+
+## Real workload to be analysed
+Ac = copy(A)
+log_info = DataFlowTasks.@log cholesky_dft!(Ac, ts)
+
+# The number of tasks being $\mathcal{O}(n^3)$, we can see how quickly the DAG
+# complexity increases (even though the test case only has 8×8 tiles here):
+
+DataFlowTasks.stack_weakdeps_env!()
+using GraphViz
+dag = GraphViz.Graph(log_info)
+
+# The critical path, highlighted in red, includes all cholesky factorizations of
+# diagonal tiles, as well as the required tasks in between them.
+
+#-
+
+# We can also readily get more details about the performance limiting factors:
+
+DataFlowTasks.describe(log_info; categories=["chol", "ldiv", "schur"])
+
+# and, at the price of loading `Makie`, display these in a more convenient
+# profile plot:
+
+using CairoMakie # or GLMakie in order to have more interactivity
+trace = plot(log_info; categories=["chol", "ldiv", "schur"])
+
+# The overhead incurred by `DataFlowTasks` seems relatively small here: the time
+# taken inserting tasks is barely measurable, and the scheduling did not lead to
+# threads waiting idly for too long. This is confirmed by the bottom middle plot,
+# showing a measured wall clock time not too much longer than the lower bound
+# obtained when suppressing idle time.
+#
+# The "Computing time: breakdown by category" plot seems to indicate that the matrix
+# multiplications performed in the "Schur" tasks account for the majority of the
+# computing time. Trying to optimize these would be a priority to increase the
+# sequential performance of the factorization.
+
+#-
+
+# # Performances
+
+# The performance of this example can be improved by using better
+# implementations for the sequential building blocks operating on tiles:
+#
+# - `LoopVectorization.jl` can improve the performance of the sequential
+#   cholesky factorization of diagonal blocks as well as the `schur_complement`
+# - `TriangularSolve.jl` provides a high-performance `ldiv!` implementation
+#
+# This approach is pursued in
+# [`TiledFactorization.jl`](https://github.com/maltezfaria/TiledFactorization),
+# where all the above mentioned building blocks are combined with the
+# parallelization strategy presented here to create a *pure Julia*
+# implementation of the matrix factorizations. The performances of this
+# implementation is assessed in the following plot, by comparison to MKL on a
+# the case of a 5000x5000 matrix decomposed in tiles of size 256x256.
+#
+# ![](cholesky_scaling.png)
+#
+# The figure above was generated by running [this
+# script](https://github.com/maltezfaria/TiledFactorization/blob/daafed7b6981853b6c71e7441fd9b212582836db/benchmarks/cholesky_scaling.sh)
+# on a machine with 2x10 Intel Xeon Silver 4114 cores (2.20GHz) with the following topology:
+#
+# ![](lfaria-precision-7920-tower-lstopo.png)
