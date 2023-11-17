aa778193d048f26a0edf2dbaacaa1bd98b3dbdc7
Luiz M. Faria <maltezfaria@gmail.com>
2023-11-17 13:48:28 +0100

    add stlibs compat

--- test/aqua_test.jl
+++ test/aqua_test.jl
@@ -3,8 +3,6 @@ using Aqua
 
 # Aqua.jl fails the toml test on 1.6 because of the [deps] entry in the
 # Project.toml file. This is a workaround.
-test_toml = VERSION >= v"1.9"
-test_toml || @warn "Skipping Aqua's `project_toml_formatting` test."
 Aqua.test_all(
     DataFlowTasks;
     ambiguities = (broken = true),
@@ -13,6 +11,6 @@ Aqua.test_all(
     project_extras = true,
     stale_deps = true,
     deps_compat = true,
-    project_toml_formatting = test_toml,
-    piracy = (; broken = true),
+    piracies = (; broken = true),
+    persistent_tasks = true,
 )
