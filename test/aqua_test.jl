229ac82a80c2ac7fc771af8649f2871cc88bf98f
Luiz M. Faria <maltezfaria@gmail.com>
2023-11-23 13:12:22 +0100

    upstream version of reverse iterator for `OrderedDict`

--- test/aqua_test.jl
+++ test/aqua_test.jl
@@ -1,16 +1,14 @@
 using DataFlowTasks
 using Aqua
 
-# Aqua.jl fails the toml test on 1.6 because of the [deps] entry in the
-# Project.toml file. This is a workaround.
 Aqua.test_all(
     DataFlowTasks;
-    ambiguities = (broken = true),
+    ambiguities = true,
     unbound_args = true,
     undefined_exports = true,
     project_extras = true,
     stale_deps = true,
     deps_compat = true,
-    piracies = (; broken = true),
+    piracies = true,
     persistent_tasks = true,
 )
